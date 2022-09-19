a097949709acf514f0e0d343ea4003c380bb8a94
KyungEun No <74607521+shruddms@users.noreply.github.com>
2022-09-19 16:36:49 +0900

    Feat : Add test

---
+++ app/src/main/java/com/kyungeun/kotestandroidsamples/Calculator.kt
@@ -0,0 +1,11 @@
+package com.kyungeun.kotestandroidsamples
+
+class Calculator {
+    fun add(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    fun sub(a: Int, b: Int): Int {
+        return a - b
+    }
+}
