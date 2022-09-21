6e2afaee9fcdf84c0977dcd79ba859440ec921b5
KyungEun No <74607521+shruddms@users.noreply.github.com>
2022-09-21 16:04:17 +0900

    Feat : Add login test code

--- app/src/main/java/com/kyungeun/kotestandroidsamples/LoginUtils.kt
+++ app/src/main/java/com/kyungeun/kotestandroidsamples/LoginUtils.kt
@@ -1,11 +1,15 @@
 package com.kyungeun.kotestandroidsamples
 
+import java.util.regex.Pattern
+
 class LoginUtils {
     fun isEmailValid(email: String): Boolean {
-        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
+        val EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$"
+        val pattern = Pattern.compile(EMAIL_PATTERN)
+        return pattern.matcher(email).matches()
     }
 
     fun isPasswordValid(password: String): Boolean {
-        return password.length > 5
+        return password.length in 8..20 && !password.contains(" ")
     }
 }
--- app/src/test/java/com/kyungeun/kotestandroidsamples/LoginUtilUnitTest.kt
+++ app/src/test/java/com/kyungeun/kotestandroidsamples/LoginUtilUnitTest.kt
@@ -1,4 +1,70 @@
 package com.kyungeun.kotestandroidsamples
 
-class LoginUtilUnitTest {
+import io.kotest.core.spec.Spec
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.core.test.TestCase
+import io.kotest.matchers.shouldBe
+
+class LoginUtilUnitTest: FunSpec() {
+    private lateinit var loginUtils: LoginUtils
+
+    override fun beforeSpec(spec: Spec) {
+        super.beforeSpec(spec)
+        loginUtils = LoginUtils()
+    }
+
+    override fun beforeTest(testCase: TestCase) {
+        super.beforeTest(testCase)
+    }
+
+    init {
+        /**
+         * email test
+         */
+        test("email is empty") {
+            loginUtils.isEmailValid("") shouldBe false
+            loginUtils.isEmailValid(" ") shouldBe false
+        }
+
+        test("email is include spaces") {
+            loginUtils.isEmailValid("i@ gmail.com") shouldBe false
+            loginUtils.isEmailValid("i@gmail. com") shouldBe false
+        }
+
+        test("email is include more than one dot") {
+            loginUtils.isEmailValid("i@gmail.com.a") shouldBe false
+        }
+
+        test("email is not include @") {
+            loginUtils.isEmailValid("gmail.com") shouldBe false
+        }
+
+        test("email format is true") {
+            loginUtils.isEmailValid("i@gmail.com") shouldBe true
+        }
+
+        /**
+         * password test
+         */
+        test("password is empty") {
+            loginUtils.isPasswordValid("") shouldBe false
+            loginUtils.isPasswordValid(" ") shouldBe false
+        }
+
+        test("password is include spaces") {
+            loginUtils.isPasswordValid("12 34567") shouldBe false
+        }
+
+        test("password length is less than 8") {
+            loginUtils.isPasswordValid("1234567") shouldBe false
+        }
+
+        test("password length is greater than 20.") {
+            loginUtils.isPasswordValid("123456789123456789123") shouldBe false
+        }
+
+        test("password format is true") {
+            loginUtils.isPasswordValid("12345678") shouldBe true
+        }
+    }
 }
